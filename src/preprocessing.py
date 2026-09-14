1cbc3cd6b7d83b940ce77d309b95f6b5d2609c97
SakshiVerma-19 <sakshi.23aiml@cambridge.edu.in>
2026-09-14 16:06:10 +0530

    Preprocessed dataset

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -1,24 +1,24 @@
 import os
 import pandas as pd
-def reconstruct_csv(input_path:str, output_path: str, target_brand:str="AmazonHelp", sample_size:int=5000):
+def reconstruct_threads(input_path:str, output_path: str, target_brand:str="AmazonHelp", sample_size:int=5000):
     """
     Parses raw TWCS dataset, extracts target brand interactions, 
     reconstructs turn-by-turn conversations, and saves a cleaned dataset.
     """
     print(f"Loading raw Dataset from {input_path}...")
-
+    cols_to_use = ['tweet_id', 'author_id', 'inbound', 'created_at', 'text', 'in_response_to_tweet_id']
     # Load required columns to save RAM
     df=pd.read_csv(input_path, usecols=cols_to_use)
-    print(f"Total raw tweets loaded: len(df)")
+    print(f"Total raw tweets loaded: {len(df)}")
 
-    df['tweet_id']=df['tweet-id'].astype(str)
-    df['in_response_to_tweet_id']=dfdf['in_response_to_tweet_id'].fillna('').astype(str).str.replace(r'\.0$', '' regex=True)
+    df['tweet_id']=df['tweet_id'].astype(str)
+    df['in_response_to_tweet_id']=df['in_response_to_tweet_id'].fillna('').astype(str).str.replace(r'\.0$', '' ,regex=True)
     tweet_dict=df.set_index('tweet_id').to_dict('index')
     print(f"Filtering dialouge threads for brand:@{target_brand}")
     reconstructed_threads=[]
     brand_tweets=df[(df['author_id']==target_brand)&(df['inbound']==False)]
 
-    for _, branch_row in branc_tweets.iterrows():
+    for _, brand_row in brand_tweets.iterrows():
         parent_id=brand_row['in_response_to_tweet_id']
         if parent_id in tweet_dict:
             customer_tweet=tweet_dict[parent_id]
@@ -41,19 +41,19 @@ def reconstruct_csv(input_path:str, output_path: str, target_brand:str="AmazonHe
     result_df = pd.DataFrame(reconstructed_threads)
     
     # Ensure directory exists before saving
-    os.makedirs(os.path.dirname(output_csv_path), exist_ok=True)
-    result_df.to_csv(output_csv_path, index=False)
+    os.makedirs(os.path.dirname(output_path), exist_ok=True)
+    result_df.to_csv(output_path, index=False)
     
     print(f"Successfully processed {len(result_df)} paired conversation threads.")
-    print(f"Cleaned output saved to: {output_csv_path}")
+    print(f"Cleaned output saved to: {output_path}")
 
 if __name__ == "__main__":
     RAW_DATA_PATH = "data/twcs.csv"
-    OUTPUT_DATA_PATH = "data/raw_sample.csv"
+    OUTPUT_DATA_PATH = "data/raw_sampled.csv"
     
     reconstruct_threads(
-        input_csv_path=RAW_DATA_PATH,
-        output_csv_path=OUTPUT_DATA_PATH,
+        input_path=RAW_DATA_PATH,
+        output_path=OUTPUT_DATA_PATH,
         target_brand="AmazonHelp",
         sample_size=5000
     )
